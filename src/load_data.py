885bff1f3c5a5f62497817da153bfdb72f1039bc
ShritejC9591 <shritejcrlsbca@gmail.com>
2024-07-11 16:50:51 +0530

    ML Model pushed to server

--- src/load_data.py
+++ src/load_data.py
@@ -1,26 +1,25 @@
-
 import os
 import yaml
 import pandas as pd
 import numpy as np
 import argparse
 #from pkgutil import get_data
-from get_data import get_data, read_params
 
-
-def load_and_save(config_path):
+def get_data(config_path):
     config = read_params(config_path)
-    df = get_data(config_path)
-    new_cols = [col.replace(" ","_") for col in df.columns]
-    #print(new_cols)
-    raw_data_path = config["load_data"]["raw_data"]
-    df.to_csv(raw_data_path, sep=",", index=False, header=new_cols)
-    return load_and_save
+    #print(config)
+    data_path = config["data_source"]["s3_data"]
+    df = pd.read_csv(data_path, sep=",", encoding='utf-8')
+    #print(df)
+    return df
 
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config=yaml.safe_load(yaml_file)
+        return config
 
-
-if __name__=="__main___":
+if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    load_and_save(config_path=parsed_args.config)
+    data = get_data(config_path=parsed_args.config)
